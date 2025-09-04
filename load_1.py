861c71cd758a0c6446c799e30ee3f29a24880174
juniorkowal <juniorkowal123@gmail.com>
2025-09-04 12:12:05 +0000

    clean

--- load_1.py
+++ load_1.py
@@ -33,47 +33,20 @@ def load_1(input_ptr : tl.tensor , N : int, D : int , output_ptr : tl.tensor , B
     tl.store(output_th, result, mask=mask_block)
 
 
-    # out = tl.load(output, mask=mask_block,other=0)
-    
-    # tgt = tgt + 17
-    # val = tl.load(tgts_ptr, mask=mask_block,other=0)
-    # val = val * 5
 
-    # tl.store(tgts_ptr, val, mask=mask_block)
-
-
-def run(kern,VEC_SIZE=10000, BLOCK_SIZE=1024, dev='cuda'):
+def run(kern, BLOCK_SIZE, dev):
     
     N, D = (500,60)
     input = torch.randn(N,D, device=dev)
     output = torch.empty_like(input, device=dev)
     total = N * D  
     grid = (triton.cdiv(total, BLOCK_SIZE),)
-   # kern[grid](input, N, D ,output, BLOCK_SIZE)
     kern[grid](input, N, D, output, BLOCK_SIZE)
      
-
-
-    # input = torch.randn(VEC_SIZE, device='cpu', dtype=torch.float32)
-    # indices = torch.randint(0, VEC_SIZE, (VEC_SIZE,), device='cpu', dtype=torch.int64)
-    # output_ref = torch.empty_like(indices, device='cpu', dtype=torch.float32)
-    # torch.gather(input, 0, indices, out=output_ref)
-   
-    # output = torch.empty_like(indices, device=dev, dtype=torch.float32)
-    # input = input.to(dev)
-    # indices = indices.to(dev)         
-    
-    # N = indices.shape[0]
-    # grid = (triton.cdiv(N, BLOCK_SIZE),)
-    # kern[grid](input, indices, output, BLOCK_SIZE, N)
-    # output=output.to('cpu')
-
-    # assert torch.allclose(output, output_ref)
     print("Pass correctness test!")        
 
 
-
-PARAM = { 'VEC_SIZE':10000, 'BLOCK_SIZE':1024 , 'dev': dev}
+PARAM = { 'BLOCK_SIZE':1024 , 'dev': dev}
 run(load_1,**PARAM)  
 
 
